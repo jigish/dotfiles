7d54abf0afdcb0fba255c94341fc7b3cbf85154a
Jigish Patel <jigish@ooyala.com>
2011-05-02 23:37:41 -0700

    Add iTerm2 config

--- config.sh
+++ config.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [[ "$TERM_PROGRAM" == "iTerm.app" ]] ; then
+  printf "\n> You appear to be running this script from within iTerm.app which could\n"
+  printf "  overwrite your new preferences on quit.\n"
+  printf "> Please quit iTerm and run this from Terminal.app or an SSH session.\n"
+  printf "  Cheers.\n\n"
+  exit 3
+fi
+
+if ps wwwaux | egrep -q 'iTerm\.app' >/dev/null ; then
+  printf "\n> You appear to have iTerm.app currently running. Please quit the\n"
+  printf "  application so your updates won't get overridden on quit.\n\n"
+  exit 4
+fi
+
 CURRDIR=pwd
 
 # Make Command-T
@@ -18,5 +32,8 @@ ln -s $CURRDIR/tigrc .tigrc
 ln -s $CURRDIR/vim-config .vim
 ln -s .vim/vimrc .vimrc
 ln -s .vim/gvimrc .gvimrc
+
+# Copy iTerm2 Configs
+cp $CURRDIR/iterm2-config/com.googlecode.iterm2.plist ~/Library/Preferences/
 
 cd $CURRDIR
