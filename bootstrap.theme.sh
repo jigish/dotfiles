24754ef317f88d633ba1be6dadaf5cbf4e317c30
Jigish Patel <jigishp@netflix.com>
2023-04-18 17:03:20 -0700

    update bootstrap theme

--- bootstrap.theme.sh
+++ bootstrap.theme.sh
@@ -22,14 +22,16 @@ else
   mkdir -p ~/.themes
   cd ~/.themes
   for style in '-bluish-accent' '-darker' '-Polar' ''; do
-    wget https://github.com/EliverLara/Nordic/releases/download/${NORDIC_VERSION}/Nordic${style}-standard-buttons-v40.tar.xz
-    wget https://github.com/EliverLara/Nordic/releases/download/${NORDIC_VERSION}/Nordic${style}-standard-buttons.tar.xz
-    wget https://github.com/EliverLara/Nordic/releases/download/${NORDIC_VERSION}/Nordic${style}-v40.tar.xz
-    wget https://github.com/EliverLara/Nordic/releases/download/${NORDIC_VERSION}/Nordic${style}.tar.xz
+    for suffix in '-standard-buttons-v40' '-standard-buttons' '-v40' ''; do
+      wget https://github.com/EliverLara/Nordic/releases/download/${NORDIC_VERSION}/Nordic${style}${suffix}.tar.xz
+      tar -xf Nordic${style}${suffix}.tar.xz
+      rm Nordic${style}${suffix}.tar.xz
+    done
   done
 
   gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
   gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
+  gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 fi
 
 
