dd0dabfd81453a80da061fc065e2dd25645671ca
Jigish Patel <jigishp@netflix.com>
2023-04-20 12:21:23 -0700

    zafiro grey

--- bootstrap.theme.sh
+++ bootstrap.theme.sh
@@ -49,8 +49,8 @@ elif [[ "${XDG_CURRENT_DESKTOP}" == *"GNOME"* ]]; then
   # Nordic theme
   echo
   echo "installing nordic theme for gnome"
-  mkdir -p ~/.themes
-  cd ~/.themes
+  mkdir -p ~/.local/share/themes
+  cd ~/.local/share/themes
   for style in '-bluish-accent' '-darker'; do # other options: '-Polar' ''
     for suffix in '-standard-buttons-v40' '-standard-buttons'; do # other options: '-v40' ''
       if [[ ! -d Nordic${style}${suffix} ]]; then
@@ -60,6 +60,11 @@ elif [[ "${XDG_CURRENT_DESKTOP}" == *"GNOME"* ]]; then
       fi
     done
   done
+  mkdir -p ~/.themes
+  cd ~/.themes
+  for f in $(ls ~/.local/share/themes); do
+    [[ ! -L ~/.local/share/themes/$f ]] && ln -s ~/.local/share/themes/$f
+  done
   gsettings set org.gnome.desktop.interface gtk-theme "Nordic-bluish-accent-standard-buttons"
   gsettings set org.gnome.desktop.wm.preferences theme "Nordic-bluish-accent-standard-buttons"
   gsettings set org.gnome.desktop.interface color-scheme prefer-dark
@@ -67,19 +72,25 @@ elif [[ "${XDG_CURRENT_DESKTOP}" == *"GNOME"* ]]; then
   # Zafiro Nord Dark (grey) Icons
   echo "installing zafiro nord dark icons"
   mkdir -p ~/.zafiro
-  [[ ! -d ~/.zafiro/icons ]] && git clone https://github.com/zayronxio/Zafiro-Nord-Dark ~/.zafiro/icons
-  cd ~/.zafiro/icons
-  git checkout . # remove my changes
+  [[ ! -d ~/.zafiro/Zafiro-Nord-Dark ]] && git clone https://github.com/zayronxio/Zafiro-Nord-Dark
+  cd ~/.zafiro/Zafiro-Nord-Dark
+  git checkout .
   git pull
-  echo "-> replacing green folder icons with grey"
-  # TODO replace green folders with grey folders
+  echo "-> replacing green folder icons with grey and name to match repo"
+  sed -i 's/Zafiro-Nord-Black/Zafiro-Nord-Dark/g' index.theme
+  find ./places -type f -exec sed -i -e 's/#a3be8c/#85a8b5/g' {} \;
+  find ./places -type f -exec sed -i -e 's/#8daf71/#7396a3/g' {} \;
+  find ./places -type f -exec sed -i -e 's/#8eac75/#7396a3/g' {} \;
+  find ./places -type f -exec sed -i -e 's/#80a264/#637279/g' {} \;
+  find ./places -type f -exec sed -i -e 's/#87a7a9/#9cb4be/g' {} \;
+  find ./places -type f -exec sed -i -e 's/#769b9d/#6f8088/g' {} \;
   mkdir -p ~/.local/share/icons
   cd ~/.local/share/icons
-  [[ ! -L Zafiro-Nord-Dark ]] && ln -s ~/.zafiro/icons/Zafiro-Nord-Dark Zafiro-Nord-Dark
+  [[ ! -L Zafiro-Nord-Dark ]] && ln -s ~/.zafiro/Zafiro-Nord-Dark
   gsettings set org.gnome.desktop.interface icon-theme 'Zafiro-Nord-Dark'
 
   # Nordzy Cursors
-  echo "installing nordzy cursors "
+  echo "installing nordzy cursors"
   mkdir -p ~/.nordzy
   [[ ! -d ~/.nordzy/cursors ]] && git clone https://github.com/alvatip/Nordzy-cursors ~/.nordzy/cursors
   cd ~/.nordzy/cursors
