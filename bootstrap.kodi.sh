69ac906fef4aaca7bf3c0189d4598c6006c79c11
Jigish Patel <jigishp@netflix.com>
2023-04-20 22:34:19 -0700

    install suggests for kodi

--- bootstrap.kodi.sh
+++ bootstrap.kodi.sh
@@ -7,6 +7,6 @@ SCRIPTDIR=$(cd `dirname $0` && pwd)
 
 sudo add-apt-repository -y ppa:team-xbmc/ppa
 sudo apt update
-sudo apt install -y kodi
+sudo apt install -y --install-suggests kodi
 
 cd $CURRDIR
