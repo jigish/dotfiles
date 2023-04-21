cf6a52924238005b808948909be9c5c0c4b20640
Jigish Patel <jigishp@netflix.com>
2023-04-21 15:05:02 -0700

    we don't install go unless we need to

--- bootstrap.go.Linux.sh
+++ bootstrap.go.Linux.sh
@@ -3,11 +3,12 @@
 [[ $UID == 0 ]] || { echo "run as root to install"; exit 1; }
 
 GO_VERSION=$(curl -sL 'https://go.dev/dl/' |grep 'linux-amd64' |grep -Eo 'go[0-9]+\.[0-9]+(\.[0-9]+){0,1}' |grep -Eo '[0-9]+\.[0-9]+(\.[0-9]+){0,1}' |sort -V -u |tail -1)
-echo "installing go${GO_VERSION}"
 
 cd /usr/local
 rm -f go
 if [[ ! -d go-${GO_VERSION} ]]; then
+  echo
+  echo "installing go${GO_VERSION}"
   curl -sLO https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
   tar -xzf go${GO_VERSION}.linux-amd64.tar.gz
   mv go go-${GO_VERSION}
