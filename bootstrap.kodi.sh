c2754b61d6f75b269ce20c7178e32f99d5875bf6
Jigish Patel <jigishp@netflix.com>
2023-04-20 22:32:42 -0700

    only install kodi

--- bootstrap.kodi.sh
+++ bootstrap.kodi.sh
@@ -7,6 +7,6 @@ SCRIPTDIR=$(cd `dirname $0` && pwd)
 
 sudo add-apt-repository -y ppa:team-xbmc/ppa
 sudo apt update
-sudo apt install -y kodi kodi-audioencoder-* kodi-pvr-*
+sudo apt install -y kodi
 
 cd $CURRDIR
