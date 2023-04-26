8aa139cfe49d3db5df4d7b40c8d7ff2f3db24bc2
Jigish Patel <jigishp@netflix.com>
2023-04-26 14:09:47 -0700

    don't use base-devel, just install everything manually

--- bootstrap.arch.sh
+++ bootstrap.arch.sh
@@ -24,9 +24,8 @@ if [[ "$?" != "0" ]]; then
   set -e
   echo
   echo "installing paru"
-  # first we need to add wheel to sudoers. why base-devel needs sudo is beyond me.
-  doas sed -i -e 's/^.*%wheel \(.*\)NOPASSWD\(.*\)$/%wheel \1NOPASSWD\2/g' /etc/sudoers
-  doas pacman -Sy --needed base-devel rustup
+  doas pacman -Sy --needed rustup ${cat ${SCRIPTDIR}/base-devel.txt}
+  [[ ! -L /usr/bin/sudo ]] && ln -s $(which doas) /usr/bin/sudo
   rustup default stable
   mkdir -p ~/tmp
   cd ~/tmp
@@ -37,13 +36,13 @@ if [[ "$?" != "0" ]]; then
   cd ~/tmp
   rm -rf paru
   cd $CURRDIR
-  doas pacman -Rs base-devel # base-devel requires sudo. we are using doas.
 else
   set -e
 fi
 cd ~/.config
 [[ ! -L paru ]] && ln -s $SCRIPTDIR/config/paru paru
 cd $CURRDIR
+[[ ! -L /usr/bin/sudo ]] && ln -s $(which doas) /usr/bin/sudo
 
 # update all the things
 echo
@@ -51,7 +50,6 @@ echo "updating / installing / cleaning packages"
 paru -Syu
 paru -S --needed $(cat ${SCRIPTDIR}/paru.txt)
 paru -c
-[[ ! -L /usr/bin/sudo ]] && ln -s $(which doas) /usr/bin/sudo
 
 # install virtualbox guest stuff in needed
 set +e
