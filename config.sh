580bc94bcb4a93087df39d89af0228a2294df1f3
Jigish Patel <jigishp@netflix.com>
2023-04-27 16:36:10 -0700

    heh

--- config.sh
+++ config.sh
@@ -13,6 +13,8 @@ export NORDIC_THEME_SUBTYPE=bluish-accent
 
 # ------------------------------------------------------------------------------------------------------------------------------------
 
+mkdir -p ${TWEAKS_DIR}
+
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
