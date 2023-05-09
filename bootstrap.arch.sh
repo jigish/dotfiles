96293db250f34baf552ea057c613f0c7f961f171
Jigish Patel <jigishp@netflix.com>
2023-05-09 14:16:10 -0700

    fix modprobe persistence

--- bootstrap.arch.sh
+++ bootstrap.arch.sh
@@ -181,7 +181,7 @@ if [[ "$?" != "0" ]]; then
   echo
   echo "enabling kbct.service"
   doas modprobe uinput
-  echo "uinput" |doas tee -a /etc/modules
+  echo "uinput" |doas tee -a /etc/modules-load.d/uinput.conf
   doas systemctl enable --now kbct.service
 else
   set -e
