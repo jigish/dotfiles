a21a1405d2e0520c7271bc5e72def04c99195227
Jigish Patel <jigish.patel@airbnb.com>
2024-01-23 23:37:30 -0800

    consistent push to quickfix

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -9,6 +9,7 @@ lspconfig.rust_analyzer.setup {}
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<C-q>', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
