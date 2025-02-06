27746fcde86ceaf404612d602051505fcbad7c3f
Jigish Patel <tskraja@gmail.com>
2025-02-05 17:04:06 -0800

    switch headline for render-markdown

--- config/nvim/lua/plugins/headlines.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "lukas-reineke/headlines.nvim",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = true,
-  },
-}
--- config/nvim/lua/theme.lua
+++ config/nvim/lua/theme.lua
@@ -106,19 +106,5 @@ require("todo-comments").setup({
   },
 })
 
--- headlines config
-require("headlines").setup({
-    markdown = {
-        headline_highlights = {
-            "Headline1",
-            "Headline2",
-            "Headline3",
-            "Headline4",
-            "Headline5",
-            "Headline6",
-        },
-        codeblock_highlight = "CodeBlock",
-        dash_highlight = "Dash",
-        quote_highlight = "Quote",
-    },
-})
+-- render-markdown config
+require("render-markdown").setup({})
